44533a65ca063ce80e6de823c8651bee662bc8bd
RedTak <jht960526@naver.com>
2025-03-04 14:53:14 +0900

    test commit
    asd

--- src/main/java/com/shinhan/zoomoney/card/ChildCardRepositiory.java
+++ src/main/java/com/shinhan/zoomoney/card/ChildCardRepositiory.java
@@ -2,5 +2,5 @@ package com.shinhan.zoomoney.card;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ChildCardRepositiory extends JpaRepository<ChildCardEntity, String> {
+public interface ChildCardRepositiory extends JpaRepository<ChildCardEntity, Integer> {
 }
