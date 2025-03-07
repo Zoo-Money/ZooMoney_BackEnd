576586d4a83859f545f9d0dc0bef248767baff71
decoyer <nockback@naver.com>
2025-03-07 10:19:59 +0900

    [fix]: 불필요한 파일 정리 및 트리 수정

--- src/main/java/com/shinhan/zoomoney/notify/NotifyRepository.java
+++ src/main/java/com/shinhan/zoomoney/notify/NotifyRepository.java
@@ -1,32 +1,27 @@
 package com.shinhan.zoomoney.notify;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotifyRepository extends JpaRepository<NotifyEntity, Integer> {
-    /*
     public List<NotifyDto> select(int member_num) {
-        // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'select'");
     }
 
     public NotifyDto selectById(int notify_num) {
-        // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'selectById'");
     }
 
     public int selectUnread(int member_num) {
-        // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'selectUnread'");
     }
 
     public void insert(NotifyDto nofityDTO) {
-        // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'insert'");
     }
 
     public void update(int notify_num) {
-        // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'update'");
     }
-    */
 }
