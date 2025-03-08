2ddcb53f81f4212b6e66c56c10a3f67aa924cb02
dmstn0526 <dmstn_0526@naver.com>
2025-03-08 14:19:05 +0900

    [feat]: 회원정보 조회 api 개발(#28)

--- src/main/java/com/shinhan/zoomoney/member/MemberRepository.java
+++ src/main/java/com/shinhan/zoomoney/member/MemberRepository.java
@@ -2,8 +2,10 @@ package com.shinhan.zoomoney.member;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MemberRepository extends JpaRepository<MemberEntity, Integer> {
 
 	MemberEntity findByMemberIdAndMemberPw(String member_id,String member_pw);
-
+	List<MemberEntity> findByMemberNum(int memberNum);
 }
--- src/main/java/com/shinhan/zoomoney/member/MemberService.java
+++ src/main/java/com/shinhan/zoomoney/member/MemberService.java
@@ -3,6 +3,8 @@ package com.shinhan.zoomoney.member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class MemberService {
 
@@ -12,5 +14,8 @@ public class MemberService {
 
     public MemberEntity login(String member_id, String member_pw) {
     	 return memberRepository.findByMemberIdAndMemberPw(member_id, member_pw);
+    }
+    public List<MemberEntity> selectByMemberNum(int member_num) {
+        return memberRepository.findByMemberNum(member_num);
     }
 }
