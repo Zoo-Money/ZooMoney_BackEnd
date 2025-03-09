91a182908dbd5527ba6cef8dbe3422f20178802c
KIMYB0402 <dydqja0402@gmail.com>
2025-03-09 22:04:17 +0900

    [feat]:회원이등록된 카드정보 API 및 파일  네이밍변경(#30)

--- src/main/java/com/shinhan/zoomoney/card/CardRepositiory.java
+++
@@ -1,6 +0,0 @@
-package com.shinhan.zoomoney.card;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CardRepositiory extends JpaRepository<CardEntity, String> {
-}
