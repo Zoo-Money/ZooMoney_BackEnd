c37f0add063e6bf07d9db8817f5af1d99077c2ce
aneunoun <117334297+aneunoun@users.noreply.github.com>
2025-03-10 15:30:00 +0900

    [feat]:QuizDTO 수정(#3)

--- src/main/java/com/shinhan/zoomoney/quiz/QuizDto.java
+++ src/main/java/com/shinhan/zoomoney/quiz/QuizDto.java
@@ -8,10 +8,17 @@ import java.sql.Date;
 @NoArgsConstructor
 @Builder
 @ToString
+@Getter
+@Setter
 public class QuizDto {
 
     private int quiz_num;
-    private int child_num;
+    private int member_num;
     private boolean quiz_check;
     private Date quiz_date;
+    
+    // 프론트로 퀴즈 데이터를 넘겨주기 위한 DTO
+    private String question;
+    private String answer;
+    private String explanation;
 }
