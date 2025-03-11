79567d4d0636cec264cee37b17364ba4fbb88ce0
RedTak <jht960526@naver.com>
2025-03-11 10:04:42 +0900

    [fix]:주식 차트 불러오기 기능 추가/#16

--- src/main/java/com/shinhan/zoomoney/stock/StockChartService.java
+++ src/main/java/com/shinhan/zoomoney/stock/StockChartService.java
@@ -22,7 +22,6 @@ public class StockChartService {
 	private final RestTemplate restTemplate = new RestTemplate();
 	private final StockChartTokenService tokenService;
 	private final StockChartRepository stockRepository;
-	private StockChartRepository stockChartRepository;
 	
 	@Value("${stock.api.key}")
 	private String apiKey;
