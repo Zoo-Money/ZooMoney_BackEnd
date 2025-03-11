9b305775663a4db9be23824a83e470bc3758eb16
Jeong Jaeyoon <jyoon3741@naver.com>
2025-03-11 11:37:41 +0900

    [fix]:누락파일생성 및 수정(#39)

--- src/main/java/com/shinhan/zoomoney/stock/StockNewsController.java
+++ src/main/java/com/shinhan/zoomoney/stock/StockNewsController.java
@@ -1,5 +1,6 @@
 package com.shinhan.zoomoney.stock;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,8 +14,13 @@ public class StockNewsController {
 	@Autowired
 	StockNewsService newsServiece;
 	
-	@GetMapping("/getnews/{query}")
-	public String searchNews(@PathVariable("query") String query) {
-		return newsServiece.searchNews(query);
+	//관련 뉴스 목록 가져오기
+	@GetMapping("/getnews/{stockName}")
+	public String searchNews(@PathVariable String stockName) {
+		String query = newsServiece.getStockName(stockName);
+		if(query != null) {
+			return newsServiece.searchNews(query);		
+		}
+		return "해당되는 뉴스를 찾을 수 없습니다." + stockName;
 	}
 }
