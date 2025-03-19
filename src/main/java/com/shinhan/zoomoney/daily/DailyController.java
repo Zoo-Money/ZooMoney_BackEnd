670ef3860490959bbcb5ab85eb56a4b9bee53c2b
aneunoun <117334297+aneunoun@users.noreply.github.com>
2025-03-19 17:02:45 +0900

    [fix]: 출석체크 param으로 변경(#103)

--- src/main/java/com/shinhan/zoomoney/daily/DailyController.java
+++ src/main/java/com/shinhan/zoomoney/daily/DailyController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.servlet.http.HttpSession;
@@ -21,27 +22,15 @@ public class DailyController {
 
 	// 출석 여부 확인 api (DB 업데이트 X)
 	@GetMapping("/status")
-	public ResponseEntity<?> checkAttendanceStatus(HttpSession session) {
-		Integer memberNum = (Integer) session.getAttribute("member_num");
-
-		if (memberNum == null) {
-			return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-					.body(Collections.singletonMap("message", "로그인이 필요합니다."));
-		}
-
+	public ResponseEntity<?> checkAttendanceStatus(@RequestParam("memberNum") int memberNum) {
+		
 		boolean isChecked = dailyService.isChecked(memberNum);
 		return ResponseEntity.ok(Collections.singletonMap("isChecked", isChecked));
 	}
 
 	// 출석체크 api (DB 업데이트 O)
 	@PostMapping("/check")
-    public ResponseEntity<?> takeAttendance(HttpSession session) {
-        Integer memberNum = (Integer) session.getAttribute("member_num");
-
-        if (memberNum == null) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                    .body(Collections.singletonMap("message", "로그인이 필요합니다."));
-        }
+    public ResponseEntity<?> takeAttendance(@RequestParam("memberNum") int memberNum) {
 
         boolean success = dailyService.markAttendance(memberNum);
         return ResponseEntity.ok(Collections.singletonMap("success", success));
