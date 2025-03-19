19be7fc33cd0331206f5575db07505024782ad6d
decoyer <nockback@naver.com>
2025-03-19 15:07:52 +0900

    [fix]: 알림 CORS 설정 제거

--- src/main/java/com/shinhan/zoomoney/notify/NotifyController.java
+++ src/main/java/com/shinhan/zoomoney/notify/NotifyController.java
@@ -9,7 +9,6 @@ import java.util.concurrent.ConcurrentHashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,8 +18,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
-// 클라이언트 도메인 허용 (CORS 설정)
-@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/notify")
 public class NotifyController {
