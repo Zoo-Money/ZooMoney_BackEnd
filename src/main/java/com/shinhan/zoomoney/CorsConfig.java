f0cafc3709328c3b772fdc1b8c923ef05bf9c387
RedTak <jht960526@naver.com>
2025-03-20 14:13:59 +0900

    [feat] 종가 조회 기능 추가

--- src/main/java/com/shinhan/zoomoney/CorsConfig.java
+++ src/main/java/com/shinhan/zoomoney/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
 
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로 허용
-                .allowedOrigins("http://localhost:3000","http://localhost:3001") // React에서 접근 허용
+                .allowedOriginPatterns("*") // React에서 접근 허용
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // HTTP 메서드 허용
                 .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true); // 쿠키, 인증 정보 허용
